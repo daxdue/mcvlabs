411ba4ec997d01eab9805ef2ee61ab003b5335fc
daxdue <ksy@picmicro.ru>
2020-10-25 23:39:42 +0300

    added lab2

---
+++ lb2/lab2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <chrono>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+//libs path for macos /usr/local/include
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv) {
+  std::cout << "Test message..." << endl;
+
+  return 1;
+}
